3d078e8d16ad878922d1ec7ff5cf3d2f7d757e1d
onejeong27 <55428400+onejeong27@users.noreply.github.com>
2019-11-18 20:58:41 +0900

    01_why am i crying

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N_CARDSET			1
+#define N_CARD				52
+#define N_DOLLAR			50
+
+
+#define N_MAX_CARDNUM		13
+#define N_MAX_USER			5
+#define N_MAX_CARDHOLD		10
+#define N_MAX_GO			17
+#define N_MAX_BET			5
+
+#define N_MIN_ENDCARD		30
+
+
+//player info
+int main(void){
+	int dollar[N_MAX_USER];						//dollars that each player has
+	int n_user;									//number of users
+
+	printf("Input the player of numbers : ");
+	scanf("%d", &n_user);
+
+}
+
