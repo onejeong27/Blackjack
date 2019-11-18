2898835a443fbf78377dcd8dfab72b782a5cee1f
onejeong27 <55428400+onejeong27@users.noreply.github.com>
2019-11-18 23:42:47 +0900

    01

---
+++ cardprocessing.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N_CARDSET			1
+#define N_CARD				52
+#define N_DOLLAR			50
+
+
+#define N_MAX_CARDNUM		13
+#define N_MAX_USER			5
+#define N_MAX_CARDHOLD		10
+#define N_MAX_GO			17
+#define N_MAX_BET			5
+
+#define N_MIN_ENDCARD		30
+
+
+//card tray object
+int CardTray[N_CARDSET*N_CARD];
+int cardIndex = 0;							
+
+
+//player info
+int dollar[N_MAX_USER];						//dollars that each player has
+int n_user;									//number of users
+
+
+//play yard information
+int cardhold[N_MAX_USER+1][N_MAX_CARDHOLD];	//cards that currently the players hold
+int cardSum[N_MAX_USER];					//sum of the cards
+int bet[N_MAX_USER];						//current betting 
+int gameEnd = 0; 							//game end flag
+
+
+
+
+
+
+int main(void){
+	
+	getCardNum();
+	printCard();
+	
+	return 0;
+}
+
+
+//card processing functions ---------------
+
+//calculate the actual card number in the blackjack game
+int getCardNum(int cardnum) {
+	int cardnum[52]={1,2,3,4,5,6,7,8,9,10,10,10,10,1,2,3,4,5,6,7,8,9,10,10,10,10,1,2,3,4,5,6,7,8,9,10,10,10,10}
+}
+
+//print the card information (e.g. DiaA)
+void printCard(int cardnum) {
+	
+}
+
+
