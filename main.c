659ff58b57716d98b6000817777c90a1c6508fdd
onejeong27 <55428400+onejeong27@users.noreply.github.com>
2019-11-18 22:31:03 +0900

    02_i gonna die.....

--- main.c
+++ main.c
@@ -15,13 +15,50 @@
 #define N_MIN_ENDCARD		30
 
 
+//card tray object
+int CardTray[N_CARDSET*N_CARD];
+int cardIndex = 0;							
+
+
 //player info
+int dollar[N_MAX_USER];						//dollars that each player has
+int n_user;									//number of users
+
+
+//play yard information
+int cardhold[N_MAX_USER+1][N_MAX_CARDHOLD];	//cards that currently the players hold
+int cardSum[N_MAX_USER];					//sum of the cards
+int bet[N_MAX_USER];						//current betting 
+int gameEnd = 0; 							//game end flag
+
+
+
+
+
+
 int main(void){
-	int dollar[N_MAX_USER];						//dollars that each player has
-	int n_user;									//number of users
-
-	printf("Input the player of numbers : ");
-	scanf("%d", &n_user);
-
+	
+	configUser();
+	betDollar();
+	
+	return 0;
 }
 
+//player settiing
+int configUser(void) {
+	printf("Input the player of numbers(Max:5) : ");
+	scanf("%d", &n_user);
+}
+
+//betting
+int betDollar(void) {
+	int my_dollar;
+	printf("your betting(total $50) = $ ");
+	scanf("%d", &my_dollar);
+	
+	int i;
+	for(i=0;i<(n_user - 1);i++)
+		dollar[i] = (rand() % 10) + 1;
+	for(i=0;i<(n_user - 1);i++)
+		printf("player %d bets $ %d\n", i+1, dollar[i]);
+}
