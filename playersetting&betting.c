615323115021be6d28426c755e3b6e39cde321ec
onejeong27 <55428400+onejeong27@users.noreply.github.com>
2019-11-20 16:12:24 +0900

    Update playersetting&betting.c

--- playersetting&betting.c
+++ playersetting&betting.c
@@ -46,8 +46,20 @@ int main(void){
 
 //player settiing
 int configUser(void) {
+	
+	do{
 	printf("Input the player of numbers(Max:5) : ");
 	scanf("%d", &n_user);
+	
+	if(n_user > N_MAX_USER)
+		printf("Too Many Players\n");
+	else if(n_user <= 0)
+		printf("Invalid Players Input\n");
+	else 
+		return n_user;
+	}
+	
+	while(n_user <= 0 || n_user > N_MAX_USER);
 }
 
 //betting
