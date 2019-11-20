669b2b5e5a78c565f0aeb1be4f594a89848f4cab
onejeong27 <55428400+onejeong27@users.noreply.github.com>
2019-11-20 21:27:33 +0900

    Update initial_card_offering&print.c

--- initial_card_offering&print.c
+++ initial_card_offering&print.c
@@ -38,15 +38,18 @@ void offerCards(void) {
 	printf("***********************************CARD OFFERING***********************************\n");
 	
 	int i;
-	//1. give two card for each players
-	for (i=0;i<n_user;i++)
-	{
-		cardhold[i][0] = pullCard();
-		cardhold[i][1] = pullCard();
-	}
-	//2. give two card for the operator
+	
 	cardhold[n_user][0] = pullCard();
+	
+	for (i=0;i<n_user;i++){
+		cardhold[i][0] = pullCard();	
+	}
+	
 	cardhold[n_user][1] = pullCard();
+
+	for (i=0;i<n_user;i++){
+		cardhold[i][1] = pullCard();	
+	}
 	
 	return;
 }
